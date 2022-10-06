2ce2bcb135dd5c364f8978b69fe08d36927c1576
Vincent Luu <luuvl@student.unimelb.edu.au>
2022-10-06 17:38:04 +1100

    Rollback previous change

--- src/ShadowDimension/ShadowDimension.java
+++ src/ShadowDimension/ShadowDimension.java
@@ -21,12 +21,35 @@ public class ShadowDimension extends AbstractGame {
 
     /* Display Constants */
     public final static int REFRESH_RATE = 60;
-    public final static int WINDOW_WIDTH = 1024, WINDOW_HEIGHT = 768;
+    private final static int WINDOW_WIDTH = 1024, WINDOW_HEIGHT = 768;
+
+    private final Image LVL0_BACKGROUND = new Image("res/background0.png");
+    private final Image LVL1_BACKGROUND = new Image("res/background1.png");
 
     /* Text and font Constants */
     private final static String FONT_FILENAME = "res/frostbite.ttf";
+    private final static int STANDARD_FONT_SIZE = 75;
+    private final Font STANDARD_TEXT = new Font(FONT_FILENAME, STANDARD_FONT_SIZE);
 
     private final static String GAME_TITLE = "SHADOW DIMENSION";
+    private final static double TITLE_X = 260, TITLE_Y = 250;
+
+    private final static int INSTRUCT_FONT_SIZE = 40;
+    private final Font INSTRUCTIONS_TEXT = new Font(FONT_FILENAME, INSTRUCT_FONT_SIZE);
+    private final static double LINE_SPACING = 45; // Drawn 45 pixels below appears to provide adequate spacing
+
+    private final static String START_INSTRUCTION = "PRESS SPACE TO START";
+    private final static double LVL0_START_X_OFFSET = 90, LVL0_START_Y_OFFSET = 190;
+    private final static double LVL1_START_X = 350, LVL1_START_Y = 350;
+
+    private final static String MOVE_CTRLS_INSTRUCT = "USE ARROW KEYS TO FIND GATE";
+    private final static String ATTACK_CTRL_INSTRUCT = "PRESS A TO ATTACK";
+    private final static String NAVEC_INSTRUCT = "DEFEAT NAVEC TO WIN";
+
+    private final static String LVL_COMPLETE = "LEVEL COMPLETE!";
+    private final static double LVL_COMPLETE_DISPLAY_TIME = 3;
+    private final static String CONGRATS = "CONGRATULATIONS!";
+    private final static String GAME_OVER = "GAME OVER!";
 
     private final static int PLAYER_HP_FONT_SIZE = 30;
     private final Font PLAYER_HP_TEXT = new Font(FONT_FILENAME, PLAYER_HP_FONT_SIZE);
@@ -49,7 +72,8 @@ public class ShadowDimension extends AbstractGame {
 
     /* Game element Constants */
     private final static Level level = new Level();
-    private final static int GAME_SCREEN = 0, GAME_PLAY = 1, GAME_WIN = 2, GAME_LOSE = 3;
+    private final static int GAME_START = 0, GAME_PLAY = 1, LVL_FINISH = 2, LVL1_START = 3;
+    private final static int GAME_WIN = -1, GAME_LOSE = -2;
     private final static double PORTAL_MIN_X = 950, PORTAL_MIN_Y = 670;
     private final static int TIMESCALE_INCREASE = 1;
     private final static int TIMESCALE_DECREASE = -1;
@@ -60,7 +84,8 @@ public class ShadowDimension extends AbstractGame {
     private final static ArrayList<Obstacle> obstacles = new ArrayList<>();
 
     /* Attributes */
-    private int gameState = GAME_SCREEN;
+    private int gameState = GAME_START;
+    private double lvlCompleteTimer = 0;
     private PlayableCharacter player;
     private Point topLeftCorner, bottomRightCorner;
 
@@ -210,6 +235,61 @@ public class ShadowDimension extends AbstractGame {
     }
 
     /**
+     * Method displays screens other than the main gameplay screen
+     * i.e. Title screen and win/lose screens
+     */
+    private void displayScreen() {
+
+        if (gameState == GAME_START) {
+
+            // Get offset required to centre the "controls" instructions relative to the "start" instructions
+            double ctrlsXOffset = INSTRUCTIONS_TEXT.getWidth(START_INSTRUCTION) / 2
+                    - INSTRUCTIONS_TEXT.getWidth(MOVE_CTRLS_INSTRUCT) / 2;
+
+            STANDARD_TEXT.drawString(GAME_TITLE, TITLE_X, TITLE_Y);
+            INSTRUCTIONS_TEXT.drawString(START_INSTRUCTION, TITLE_X + LVL0_START_X_OFFSET,
+                    TITLE_Y + LVL0_START_Y_OFFSET);
+            INSTRUCTIONS_TEXT.drawString(MOVE_CTRLS_INSTRUCT,
+                    TITLE_X + LVL0_START_X_OFFSET + ctrlsXOffset, TITLE_Y + LVL0_START_Y_OFFSET + LINE_SPACING);
+
+        } else if (gameState == LVL_FINISH) {
+
+            lvlCompleteTimer += 1.0/REFRESH_RATE;
+            double xPos = WINDOW_WIDTH/2.0 - STANDARD_TEXT.getWidth(LVL_COMPLETE)/2.0;
+            double yPos = WINDOW_HEIGHT/2.0 + STANDARD_FONT_SIZE/2.0;
+            STANDARD_TEXT.drawString(LVL_COMPLETE, xPos, yPos);
+
+        } else if (gameState == LVL1_START) {
+
+            INSTRUCTIONS_TEXT.drawString(START_INSTRUCTION, LVL1_START_X, LVL1_START_Y);
+
+            double attackCtrlXOffset = INSTRUCTIONS_TEXT.getWidth(START_INSTRUCTION) / 2
+                    - INSTRUCTIONS_TEXT.getWidth(ATTACK_CTRL_INSTRUCT) / 2;
+            INSTRUCTIONS_TEXT.drawString(ATTACK_CTRL_INSTRUCT,
+                    LVL1_START_X + attackCtrlXOffset, LVL1_START_Y + LINE_SPACING);
+
+            double navecInstructXOffset = INSTRUCTIONS_TEXT.getWidth(START_INSTRUCTION) / 2
+                    - INSTRUCTIONS_TEXT.getWidth(NAVEC_INSTRUCT) / 2;
+            INSTRUCTIONS_TEXT.drawString(NAVEC_INSTRUCT,
+                    LVL1_START_X + navecInstructXOffset, LVL1_START_Y + 2*LINE_SPACING);
+
+        } else if (gameState == GAME_WIN) {
+
+            double xPos = WINDOW_WIDTH/2.0 - STANDARD_TEXT.getWidth(CONGRATS)/2.0;
+            double yPos = WINDOW_HEIGHT/2.0 + STANDARD_FONT_SIZE/2.0;
+            STANDARD_TEXT.drawString(CONGRATS, xPos, yPos);
+
+        } else if (gameState == GAME_LOSE) {
+
+            double xPos = WINDOW_WIDTH/2.0 - STANDARD_TEXT.getWidth(GAME_OVER)/2;
+            double yPos = WINDOW_HEIGHT/2.0 + STANDARD_FONT_SIZE/2.0;
+            STANDARD_TEXT.drawString(GAME_OVER, xPos, yPos);
+
+        }
+
+    }
+
+    /**
      * Method displays the main gameplay screen
      */
     private void displayGame() {
@@ -265,24 +345,26 @@ public class ShadowDimension extends AbstractGame {
             Demon.changeSpeedMultiplier(TIMESCALE_DECREASE);
         }
 
-//        if (gameState == GAME_START && input.wasPressed(Keys.W)) {
-//            level.skipToEnd();
-//            initializeGameObjects();
-//            gameState = GAME_PLAY;
-//        } else
-        if (gameState == GAME_SCREEN && input.wasPressed(Keys.SPACE)) {
+        if (gameState == GAME_START && input.wasPressed(Keys.W)) {
+            level.skipToEnd();
+            initializeGameObjects();
+            gameState = GAME_PLAY;
+        } else if ((gameState == GAME_START || gameState == LVL1_START) && input.wasPressed(Keys.SPACE)) {
             level.nextLevel();
             initializeGameObjects();
             gameState = GAME_PLAY;
-        // Level 0 win condition: Player position is in the portal
+        } else if (gameState == LVL_FINISH && lvlCompleteTimer >= LVL_COMPLETE_DISPLAY_TIME) {
+            level.nextLevel();
+            gameState = LVL1_START;
+            // Level 0 win condition: Player position is in the portal
         } else if (gameState == GAME_PLAY && level.getCurrentLevel() == 0
                 && player.centre().x >= PORTAL_MIN_X && player.centre().y >= PORTAL_MIN_Y) {
-            gameState = GAME_SCREEN;
+            gameState = LVL_FINISH;
             level.nextLevel();
-        // Level 1 win condition: Navec is exhausted (i.e. has been killed)
+            // Level 1 win condition: Navec is exhausted (i.e. has been killed)
         } else if (gameState == GAME_PLAY && level.getCurrentLevel() == 1 && enemies.get(NAVEC_INDEX).isExhausted()) {
             gameState = GAME_WIN;
-        // Lose condition: Player HP reaches its minimum
+            // Lose condition: Player HP reaches its minimum
         } else if (gameState == GAME_PLAY && player.getHP() == PlayableCharacter.getMinHP()) {
             gameState = GAME_LOSE;
         }
@@ -304,7 +386,7 @@ public class ShadowDimension extends AbstractGame {
             displayGame();
             updateEnemyStates();
         } else {
-            level.displayScreen(gameState);
+            displayScreen();
         }
 
         if (input.wasPressed(Keys.ESCAPE)) {
